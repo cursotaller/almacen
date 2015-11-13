00f88034f1035ac4f715adc32da1b73443cc9371
asullom <asullom@gmail.com>
2015-11-13 15:29:04 -0500

    mvc

---
+++ almacen_web/app/controllers/ctll.js
@@ -0,0 +1,31 @@
+app
+
+.controller("ctll", function ($scope) {
+		//$scope.nombre="Miguel";
+		$scope.saludo = function  () {
+			console.log("Hola "+$scope.nombre);
+		}
+		$scope.list=[
+					    {
+					        "id": 2,
+					        "codigo": "02",
+					        "nombre": "Laptop",
+					        "activo": true,
+					        "fecha": "2015-11-08T15:31:08Z"
+					    },
+					    {
+					        "id": 3,
+					        "codigo": "Electro",
+					        "nombre": "Electrodomesticos",
+					        "activo": false,
+					        "fecha": null
+					    },
+					    {
+					        "id": 4,
+					        "codigo": "Electro",
+					        "nombre": "Electrodomesticos",
+					        "activo": false,
+					        "fecha": null
+					    }
+					]
+	});
---
+++ almacen_web/app/router.js
@@ -0,0 +1,10 @@
+app
+
+.config(function ($routeProvider) {
+	// body...
+	$routeProvider.when("/saludo",
+	{
+		"templateUrl": "app/views/saludo.html"
+	}
+	);
+});
